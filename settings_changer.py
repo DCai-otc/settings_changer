945df436bca8e5a4243c5f58758e3e12a99b2a11
Dcai_otc <caid@stu.otc.school.nz>
2020-08-06 18:43:48 +1200

    Version 0.3 - Fixed bugs  - Fixed Data Centers not working  - Fixed Sensitivity Multiplier not working  - Fixed Incorrect file directory for some users. However this fix    increased loading times when saving. Will be fixed next time.

--- settings_changer.py
+++ settings_changer.py
@@ -1,6 +1,10 @@
-# actual program, no ui or ways to interact yet
+from tkinter import * # Basic Setup
+UniFont = "Arial"
 
+# actual program, no ui or ways to interact yet
 import os, fileinput, time
+
+
 #defines function which finds the file
 def FoldersIn(Path):
 
@@ -8,23 +12,19 @@ def FoldersIn(Path):
 
     for r, d, f in os.walk(Path):
         for folder in d:
-            if "Pictures\Documents\My Games\Rainbow Six - Siege" in str(os.path.join(r, folder)):
+            if "Documents\My Games\Rainbow Six - Siege" in str(os.path.join(r, folder)):
                 for r, d, f in os.walk(str(os.path.join(r, folder))):
                     for folder in d:
                         if "Benchmark" not in (str(os.path.join(r, folder))):
                             Folders.append(str(os.path.join(r, folder)) + "/GameSettings.ini")
                 break
-
     return Folders
 
-#asks for the user's input
 
+def SaveFPS(MaxFPS): # save fps
 
-
-#finds the different lines storing different settings with a function, then change it.
-def UpdateGameFiles(MaxFPS, DataCenter, SensitivityMultiplier):
-
-    global Folders
+    Folders = FoldersIn("C:/Users/")
+    print (Folders)
 
     for i in range(len(Folders)):
 
@@ -32,36 +32,115 @@ def UpdateGameFiles(MaxFPS, DataCenter, SensitivityMultiplier):
             data = file.readlines()
 
         try:
-            data[104] = 'FPSLimit=' + str(MaxFPS) + "\n"
+            data[104] = 'FPSLimit={}\n'.format(MaxFPS)
+
         except:
             pass
-
-
-        try:
-            data[148] = 'DataCenterHint=' + str(DataCenter) + "\n"
-        except:
-            pass
-
-
-        try:
-            data[84] = 'MouseSensitivityMultiplierUnit=' + str(SensitivityMultiplier) + "\n"
-        except:
-            pass
-
 
         with open(Folders[i], 'w') as file:
             file.writelines(data)
 
-def SaveSettings(MaxFPS, DataCenter, SensitivityMultiplier):
-
-    global Folders
+def DataCenter(DataCenter): #save data center
 
     Folders = FoldersIn("C:/Users/")
 
-    UpdateGameFiles(MaxFPS, DataCenter, SensitivityMultiplier)
+    for i in range(len(Folders)):
 
-MaxFPS = str(input("What would you like to change the FPS limit to? Anything below 30 will disable the FPS limit."))
-DataCenter = (input("What would you like to change the Data Center to?"))
-SensitivityMultiplier = str(input("What would you like to change the Sensitivity Multiplier to?"))
+        with open(Folders[i], 'r') as file:
+            data = file.readlines()
 
-SaveSettings(MaxFPS, DataCenter, SensitivityMultiplier)
+        try:
+            data[149] = 'DataCenterHint=' + str(DataCenter) + "\n"
+        except:
+            pass
+
+        with open(Folders[i], 'w') as file:
+            file.writelines(data)
+
+def MouseSens(MouseSens):#saves mouse sensitivity
+
+    Folders = FoldersIn("C:/Users/")
+
+    for i in range(len(Folders)):
+
+        with open(Folders[i], 'r') as file:
+            data = file.readlines()
+
+        try:
+            data[85] = 'MouseSensitivityMultiplierUnit=' + str(MouseSens) + "\n"
+        except:
+            pass
+
+        with open(Folders[i], 'w') as file:
+            file.writelines(data)
+
+
+
+
+
+# Creating and setting up a window
+Window = Tk()
+Window.state("zoomed")
+Window.title("Modifi for Rainbow Six: Siege")
+Window.config(bg="gray13")
+
+# button 1: FPS
+LabelWidget = Label(Window, text="FPS Limit", font = (UniFont, 60), bg="gray13", fg="gray87")
+LabelWidget.place(relx=0.15, rely=0.5, anchor = CENTER)
+
+FPSWidget = Entry(Window, font = (UniFont, 20), bg="gray19", fg="gray80")
+FPSWidget.place(relx=0.15, rely = 0.6, anchor = CENTER)
+
+SaveButton = Button(Window, font = (UniFont, 20), bg="gray19", fg="gray87", command = lambda: SaveFPS(FPSWidget.get()), text="Save", relief = 'flat', bd=0)
+SaveButton.place(relx=0.257, rely = 0.6, anchor = CENTER)
+
+#button 2: server
+LabelWidget = Label(Window, text="Server ", font = (UniFont, 60), bg="gray13", fg="gray87")
+LabelWidget.place(relx=0.4, rely=0.5, anchor = CENTER)
+
+ServerWidget = Entry(Window, font = (UniFont, 20), bg="gray19", fg="gray80")
+ServerWidget.place(relx=0.4, rely = 0.6, anchor = CENTER)
+
+SaveServer = Button(Window, font = (UniFont, 20), bg="gray19", fg="gray87", command = lambda: DataCenter(ServerWidget.get()), text="Save", relief = 'flat', bd=0)
+SaveServer.place(relx=0.515   , rely = 0.6, anchor = CENTER)
+
+#button 3: Sensitivity Multiplier
+LabelWidget = Label(Window, text="Multiplier", font = (UniFont, 60), bg="gray13", fg="gray87")
+LabelWidget.place(relx=0.7, rely=0.5, anchor = CENTER)
+
+MouseWidget = Entry(Window, font = (UniFont, 20), bg="gray19", fg="gray80")
+MouseWidget.place(relx=0.7, rely = 0.6, anchor = CENTER)
+
+SaveSens = Button(Window, font = (UniFont, 20), bg="gray19", fg="gray87", command = lambda: MouseSens(MouseWidget.get()), text="Save", relief = 'flat', bd=0)
+SaveSens.place(relx=0.815   , rely = 0.6, anchor = CENTER)
+
+
+
+
+
+
+
+
+#testing, ignore
+mainframe = Frame(Window)
+mainframe.grid(column=0,row=0, sticky=(N,W,E,S) )
+mainframe.columnconfigure(0, weight = 1)
+mainframe.rowconfigure(0, weight = 1)
+mainframe.pack(pady = 100, padx = 100)
+
+tkvar = StringVar(Window)
+choices = { 'Pizza','Lasagne','Fries','Fish','Potatoe'}
+tkvar.set('Pizza') # set the default option
+
+popupMenu = OptionMenu(mainframe, tkvar, *choices)
+Label(mainframe, text="Choose a dish").grid(row = 1, column = 1)
+popupMenu.grid(row = 2, column =1)
+
+# on change dropdown value
+def change_dropdown(*args):
+    print( tkvar.get() )
+
+# link function to change dropdown
+tkvar.trace('w', change_dropdown)
+
+Window.mainloop()
